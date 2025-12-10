9dc91b8f125dcb77ede709d1f98b3825aa9c754f
shaik karimulla <karimulla9673@gmail.com>
2025-12-10 19:49:35 +0530

    fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
   },
   server: {
     port: 5173,
