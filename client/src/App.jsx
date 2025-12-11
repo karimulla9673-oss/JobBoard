565245de25b26a49727e49582f61c308f0b35c8b
shaik karimulla <karimulla9673@gmail.com>
2025-12-11 20:35:18 +0530

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
+
 import Home from './pages/Home';
-import JobDetails from './pages/JobDetails';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Terms from './pages/Terms';
+import JobDetails from './pages/JobDetails';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 import './App.css';
 
 const queryClient = new QueryClient({
@@ -25,22 +28,32 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        
         <Navbar />
+
         <main style={{ flex: 1 }}>
           <Routes>
+
+            {/* Main Routes */}
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacy" element={<PrivacyPolicy />} />
             <Route path="/terms" element={<Terms />} />
-            <Route path="/jobs" element={<Home />} />
-            <Route path="/jobs/:id/:slug?" element={<JobDetails />} />
+
+            {/* Correct Dynamic Route */}
+            <Route path="/jobs/:id/:slug" element={<JobDetails />} />
+
+            {/* Catch-all route (always last) */}
             <Route path="*" element={<Home />} />
+
           </Routes>
         </main>
+
         <Footer />
+
+        <Toaster />
       </div>
-      <Toaster />
     </QueryClientProvider>
   );
 }
